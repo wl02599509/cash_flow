5fcd9453cd9c4afed7d9dbd114977c1663c0b2cb
Mike <wl02599509@gmail.com>
2022-08-27 17:07:29 +0800

    僅有使用信用卡設定

--- office_api_worked.rb
+++ office_api_worked.rb
@@ -8,12 +8,13 @@ a = {
   "TradeDesc": "促銷方案", 
   "PaymentType": "aio", 
   "MerchantTradeDate": "2013/03/12 15:30:23", 
-  "MerchantTradeNo": "ecpay20220825534", 
+  "MerchantTradeNo": "ecpay20220828014", 
   "MerchantID": 2000132, 
   "ReturnURL": "https://www.ecpay.com.tw/receive.php",
   "ItemName": "Apple iphone 7 手機殼", 
   "TotalAmount": 1000, 
   "ChoosePayment": "ALL", 
+  "IgnorePayment": "WebATM#ATM#CVS#BARCODE",
   "EncryptType": 1
 }
 
